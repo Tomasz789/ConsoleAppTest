ea4639e61871dc20ba61e5071ceba6276c738248
Tomasz789 <lachowiec_95@wp.pl>
2023-07-29 10:11:51 +0200

    Text lib class

---
+++ TextConsoleApp/TextApp.Lib/TextClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextApp.Lib
+{
+    public static class TextClass
+    {
+        public static int CountUnequal(string text)
+        {
+            return 0;
+        }
+    }
+}

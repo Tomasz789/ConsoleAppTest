93f67281088cbf309cb24f0bc55db2ecb995cac0
Tomasz789 <lachowiec_95@wp.pl>
2023-07-29 13:01:10 +0200

    upload

---
+++ TextConsoleApp/TextApp.Tests/TextTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextApp.Lib;
+
+namespace TextApp.Tests
+{
+    [TestFixture]
+    public class TextTests
+    {
+        [Test]
+        public void CheckIfStringNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => TextClass.CountUnequal(null));
+        }
+    }
+}
